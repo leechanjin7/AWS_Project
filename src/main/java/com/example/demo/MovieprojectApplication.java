a676f5e05c33c97fd4f9290b25eb2ca243e7d28b
unknown <chanjini7@naver.com>
2023-10-25 08:50:47 +0900

    AWS

--- src/main/java/com/example/demo/MovieprojectApplication.java
+++ src/main/java/com/example/demo/MovieprojectApplication.java
@@ -8,18 +8,10 @@ import java.io.IOException;
 @SpringBootApplication
 public class MovieprojectApplication {
 
-<<<<<<< HEAD
 	public static void main(String[] args) {
 		SpringApplication.run(MovieprojectApplication.class, args);
 	}
 
 
 }
-=======
-    public static void main(String[] args) {
-        SpringApplication.run(MovieprojectApplication.class, args);
-    }
-	
 
-}
->>>>>>> 01a88a180a5510c1c6b235a8f804e5ae0076975b
